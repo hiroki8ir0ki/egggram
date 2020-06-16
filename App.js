cd5756baba5ed43dff3cb2af2908a70fe29929a4
room665 <hiroki@HirokinoMacBook-Pro.local>
2020-06-16 16:45:39 +0900

    register

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import * as firebase from "firebase";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+var firebaseConfig = {
+  apiKey: "AIzaSyCvtAAamGXDEvxYYOBypoE8xTESHX_irX8",
+  authDomain: "egggram-12710.firebaseapp.com",
+  databaseURL: "https://egggram-12710.firebaseio.com",
+  projectId: "egggram-12710",
+  storageBucket: "egggram-12710.appspot.com",
+  messagingSenderId: "491282191001",
+  appId: "1:491282191001:web:6c5175446e8bab35eade08",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const AppStack = createStackNavigator({
+  Home: HomeScreen,
 });
+
+const AuthStack = createStackNavigator({
+  Register: RegisterScreen,
+  Login: LoginScreen,
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppStack,
+      Auth: AuthStack,
+    },
+    {
+      initialRouterName: "Loading",
+    }
+  )
+);
